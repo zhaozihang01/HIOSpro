4c8230e8d1966546222f26b66021e493382989d1
zhaozihang01 <zhaozihang01@gmail.com>
2026-07-17 01:03:14 +0900

    创建Header组件

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        background: "#0b2a4a",
+        color: "white",
+        padding: "22px",
+        borderRadius: "16px",
+        marginBottom: "30px",
+      }}
+    >
+      <h1
+        style={{
+          margin: 0,
+          fontSize: "34px",
+        }}
+      >
+        HIOS Morning Research
+      </h1>
+
+      <p
+        style={{
+          marginTop: "8px",
+          opacity: 0.85,
+        }}
+      >
+        Professional Investment Research Platform
+      </p>
+    </header>
+  );
+}
