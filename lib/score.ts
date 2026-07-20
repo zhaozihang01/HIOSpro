f68381e644d68ed5fdcaa3b5431a33956f0f5bde
zhaozihang01 <zhaozihang01@gmail.com>
2026-07-20 19:28:28 +0900

    创建 HIOS Score 统一入口

---
+++ lib/score.ts
@@ -0,0 +1,11 @@
+export {
+  calculateHiosScore,
+  type HiosAnalysis,
+  type HiosScoreBreakdown,
+} from "@/lib/hiosEngine";
+
+export {
+  getHiosScore,
+  getRealHiosScore,
+  type HiosRating,
+} from "@/lib/hiosScore";
