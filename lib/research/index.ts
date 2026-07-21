c66ec7b10bfd439b17657cf7f90b112a13a655b3
zhaozihang01 <zhaozihang01@gmail.com>
2026-07-21 15:39:39 +0900

    Create index.ts

---
+++ lib/research/index.ts
@@ -0,0 +1,28 @@
+export {
+  generateStockResearch,
+} from "./service";
+
+export {
+  evaluateTrend,
+} from "./trend";
+
+export type {
+  TrendEvaluation,
+} from "./trend";
+
+export {
+  evaluateRisk,
+} from "./risk";
+
+export type {
+  RiskEvaluation,
+} from "./risk";
+
+export type {
+  ResearchTrend,
+  ResearchRisk,
+  ResearchSignal,
+  ResearchScore,
+  ResearchReason,
+  StockResearchResult,
+} from "./types";
